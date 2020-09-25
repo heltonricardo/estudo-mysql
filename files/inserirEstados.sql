b63856ec786645f5f75769d9c0ebcb0489a73e8f
heltonr13 <50843386+heltonr13@users.noreply.github.com>
2020-09-25 09:41:43 -0300

    Create inserirEstados.sql

---
+++ files/inserirEstados.sql
@@ -0,0 +1,9 @@
+INSERT INTO ESTADOS (NOME, SIGLA, REGIAO, POPULACAO)
+VALUES ('Acre', 'AC', 'Norte', 0.83);
+
+INSERT INTO ESTADOS
+    (NOME, SIGLA, REGIAO, POPULACAO)
+VALUES
+    ('Alagoas', 'AL', 'Nordeste', 3.38),
+    ('Amapá', 'AP', 'Norte', 0.8),
+    ('Amazonas', 'AM', 'Norte', 4.06);
